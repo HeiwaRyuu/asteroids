3c49aa340a84c8d04dba686217a4ce0f655483b6
Heiwaryuu <vitor.carvalho.ufu@gmail.com>
2024-11-04 08:28:46 -0300

    Adding clock max 60 FPS

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip()
+        # Delta
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
